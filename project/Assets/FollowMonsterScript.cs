a2815ed3c03d987b419a9e76b61d6385d84cd8eb
Monika <monika.klesk@gmail.com>
2015-06-16 19:35:38 +0200

    Added walking monster

---
+++ project/Assets/FollowMonsterScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMonsterScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform monster;
+	Vector3 translate;
+	void Start () {
+		translate = monster.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = monster.position - translate;
+	}
+}
---
+++ project/Assets/WalkScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkScript : MonoBehaviour {
+
+	public float movementSpeed = 10;
+	public SpriteRenderer ren;
+
+	void set()
+	{
+		ren = transform.Find ("Cube").GetComponent<SpriteRenderer>();
+	}
+
+	void Update(){
+
+		transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+		//Vector2 v = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		//v *= 20;
+		//rigidbody.AddForce(v);
+	}
+}
